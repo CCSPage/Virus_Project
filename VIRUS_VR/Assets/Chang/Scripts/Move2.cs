29d660f0659946096a16b70d87e3c8465b080f6d
Chang <hahnc589@newschool.edu>
2020-04-11 13:14:45 -0400

    Chang

--- VIRUS_VR/Assets/Chang/Scripts/Move2.cs
+++ VIRUS_VR/Assets/Chang/Scripts/Move2.cs
@@ -5,21 +5,23 @@ using UnityEngine;
 public class Move2 : MonoBehaviour
 {
     public int ran;
+    public float speed = 1.5f;
     public Collider[] checkpoints;
    // int layerID = 8;
     int layerMask = 1 << 8;
 
-    private float timer =0.5f;
+   // private float timer =0.5f;
     
     void Start()
     {
         checkpoints = Physics.OverlapSphere(this.transform.position, 15f, layerMask);
         ran = Random.Range(0, checkpoints.Length);
         Debug.Log(ran);
+        StartCoroutine(NumberGen());
     }
     void Update()
     {
-        if (timer < 0)
+       /* if (timer < 0)
         {
             ran = Random.Range(0, checkpoints.Length);
             Debug.Log("d");
@@ -28,10 +30,11 @@ public class Move2 : MonoBehaviour
         else if (timer <= 1)
         {
             timer -= Time.deltaTime;
-        }
-        if (checkpoints.Length >0) { this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, checkpoints[ran].transform.position, 0.8f * Time.deltaTime); }
+        }*/
+        if (checkpoints.Length >0) { this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, checkpoints[ran].transform.position, speed * Time.deltaTime); }
          if (gameObject.transform.position == checkpoints[ran].transform.position) {
            ran = Random.Range(0, checkpoints.Length);
+            Debug.Log(ran);
           }
     }
     private void FixedUpdate()
@@ -49,5 +52,14 @@ public class Move2 : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(this.transform.position, 15f);
+    }
+
+    IEnumerator NumberGen()
+    {
+        while (true)
+        {
+            ran = Random.Range(0, checkpoints.Length);
+            yield return new WaitForSeconds(5);
+        }
     }
 }
