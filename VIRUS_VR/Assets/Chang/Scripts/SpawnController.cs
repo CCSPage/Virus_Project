eb50ba1272755d33a1d65128a0d717b62e481d13
Chang <hahnc589@newschool.edu>
2020-04-11 02:20:57 -0400

    Chang

---
+++ VIRUS_VR/Assets/Chang/Scripts/SpawnController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public Transform[] spawnLocations;
+    public GameObject[] Cells;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ VIRUS_VR/Assets/Chang/Scripts/Virus.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Virus : MonoBehaviour
+{
+    public Transform armPos;
+    private bool enter = false;
+    void Start()
+    {
+        transform.rotation = Quaternion.Euler(Random.Range(0f, 300), Random.Range(0f, 300), Random.Range(0f, 300));
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enter)
+        {
+            gameObject.transform.position = armPos.position;
+        }
+
+        if (transform.parent != null) {
+            this.gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, transform.parent.position,0.015f*Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            enter = true;
+            Debug.Log("dfsd");
+            gameObject.transform.parent = null;
+            
+            Destroy(this.gameObject, 1f);
+        }
+    }
+}
