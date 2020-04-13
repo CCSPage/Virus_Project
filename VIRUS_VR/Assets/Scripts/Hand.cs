e821378a111ec55b49ae9160d5e7cea370bde609
hoanm825 <54957396+hoanm825@users.noreply.github.com>
2020-04-13 17:21:22 +0700

    my

---
+++ VIRUS_VR/Assets/Scripts/Hand.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    static public bool grabBool = false;
+    private Collider col;
+    void Start()
+    {
+        col = GetComponent<Collider>();
+    }
+
+    void Update()
+    {
+        if (grabBool) {
+            col.enabled = false;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Virus")
+        {
+            grabBool = true;
+            col.enabled = false;
+            Debug.Log("FF");
+        }
+        else 
+        {
+            grabBool = false;
+            
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        grabBool = false;
+        Debug.Log("FF");
+    }
+}
